147721ff2d3a9133e5d9545cd5f4e2c6d1a689f5
Shibam Biswas <122299216+shibam05@users.noreply.github.com>
2024-10-14 17:12:07 +0000

    Table form

--- Basic/test.js
+++ Basic/test.js
@@ -1,2 +1,11 @@
 const currentDate = new Date();
 console.log(currentDate);
+
+const username = "sbiswas";
+let email = "sbiswas2@gmail.com";
+/*
+var
+*/
+password = "jsxnodebun";
+let city;
+console.table([username, email, password, city]);
