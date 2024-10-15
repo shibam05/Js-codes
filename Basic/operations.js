aeb209e822bbd92dd06eaeb8d351d012c00997c4
Shibam Biswas <sbiswas2004in@gmail.com>
2024-10-15 20:04:37 +0530

    operations

---
+++ Basic/operations.js
@@ -0,0 +1,9 @@
+let s1 = "hello", s2 = "programmer";
+console.log(s1 + s2)
+console.log("1" + 2); // 12
+console.log("1" + 2 + 3); // 123
+console.log(1 + 2 + "3"); // 33
+console.log(+true); // 1
+console.log(+""); // 0
+
+// increment operator
