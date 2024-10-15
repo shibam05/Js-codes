16e586f1e2c15ac8dfb786f76fe5665d5ff0f692
Shibam Biswas <sbiswas2004in@gmail.com>
2024-10-15 20:34:34 +0530

    data types

---
+++ Basic/data_types.js
@@ -0,0 +1,25 @@
+// Primitive
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+/* Returntypes
+
+Number => number
+String  => string
+Boolean  => boolean
+null  => object
+undefined  =>  undefined
+Symbol  =>  symbol
+BigInt  =>  bigint
+*/
+
+
+// Non-primitive (reference)
+// Array, Object, Function
+
+/* Returntypes
+
+Arrays  =>  object
+Function  =>  function
+Object  =>  object
+*/
+
